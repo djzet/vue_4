0313a74b0c2eb3e45cb363691374ac83d8003676
gr411_sieal <silaevegor2003@mail.ru>
2023-03-18 12:26:10 +0700

    create home

---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home-page">
+    <app-banner></app-banner>
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-9">
+          <app-products :api-url="apiUrlProd"></app-products>
+        </div>
+        <div class="col-md-3">
+          <app-cart :api-url="apiUrlCart"></app-cart>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import AppBanner from "@/components/Banner.vue";
+import AppProducts from "@/components/Products.vue";
+import AppCart from "@/components/Cart.vue";
+export default {
+  name: "AppHome",
+  components: {
+    AppCart,
+    AppProducts,
+    AppBanner,
+  },
+  data() {
+    return {
+      apiUrlProd: "/products",
+      apiUrlCart: "/cart",
+    };
+  },
+};
+</script>

dcdc7a06207fbf139a1bb5c011b5cc25780108eb
gr411_sieal <silaevegor2003@mail.ru>
2023-03-18 12:31:05 +0700

    create banner

---
+++ src/components/Banner.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="banner">
+    <div class="container">
+      <h1>App Shop</h1>
+      <p>Интернет магазин</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppBanner",
+};
+</script>

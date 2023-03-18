3ce971c76d1db282c7546f492927f3ceadd31fb9
gr411_sieal <silaevegor2003@mail.ru>
2023-03-18 12:31:41 +0700

    create cart

---
+++ src/components/Cart.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <app-loading v-if="isLoading"></app-loading>
+    <div v-if="data">
+      <div class="article-preview" v-for="prod in cart.data" :key="prod.id">
+        <div class="article-meta">
+          <h2>{{ prod.name }}</h2>
+          <div class="article-meta">
+            <router-link @click="addProduct" :to="{ name: 'home' }" v-if="this.$store.state.token">
+              <h3>Цена: {{ prod.price }}</h3>
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import queryString from "query-string";
+import { actionTypes } from "@/store";
+import AppLoading from "@/components/Loading.vue";
+import axios from "axios";
+
+export default {
+  name: "AppCart",
+  components: {
+    AppLoading,
+  },
+  props: {
+    apiUrl: {
+      type: String,
+      required: true,
+    },
+  },
+  data(){
+    return{
+      cart: [],
+    }
+  },
+  computed: {
+    ...mapState({
+      isLoading: (state) => state.isLoading,
+      data: (state) => state.data,
+    }),
+    baseUrl() {
+      return this.$route.path;
+    },
+  },
+  watch: {
+    apiUrl() {
+      this.fetchFeed();
+    },
+  },
+
+  mounted() {
+      axios.get(this.$store.state.api + '/cart').then((response)=>{
+        this.cart = response.data
+      })
+    this.fetchFeed();
+  },
+  methods: {
+    addProduct(){
+
+    },
+    fetchFeed() {
+      const parsedUrl = queryString.parseUrl(this.apiUrl);
+      const stringifiedParams = queryString.stringify({
+        ...parsedUrl.query,
+      });
+      const apiUrlWithParams = `${parsedUrl.url}?${stringifiedParams}`;
+      this.$store.dispatch(actionTypes.getProducts, {
+        apiUrl: apiUrlWithParams,
+      });
+    },
+  },
+};
+</script>
